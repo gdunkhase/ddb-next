2e1a9c0817374556a37aec5a72d79f15af41e534
Ar Brahaj <armand.brahaj@fiz-karlsruhe.de>
2012-12-19 15:15:59 +0100

    Supporting all the news retrieved as params

--- grails-app/controllers/de/ddb/next/ContentController.groovy
+++ grails-app/controllers/de/ddb/next/ContentController.groovy
@@ -12,11 +12,14 @@ class ContentController {
 		def lang = "de"
 		def url = "http://141.66.130.151:8003"
 		def path = "/static/"+lang+"/news/index.html"
+		if (params.id!=null){
+			path = "/static/"+lang+"/news/"+params.id+".html"
+		}
 		def query = [ client: "DDB-NEXT" ]
-
+		println path
 		// Submit a request via GET
 		def response = ApiConsumer.getText(url, path, query)
-		
+
 		if (response == "Not found"){
 			redirect(controller: "error", action: "notfound")
 		}
@@ -32,9 +35,9 @@ class ContentController {
 		def robot = fetchRobots(content)
 		def body = fetchBody(content)
 		return [title:title, author:author, keywords:keywords,robot:robot,content:body]
-		
+
 	}
-	
+
 	private def fetchBody(content) {
 		def bodyMatch = content =~ /(?s)<body>(.*?)<\/body>/
 		return bodyMatch[0][1]
@@ -42,21 +45,25 @@ class ContentController {
 
 	private fetchAuthor(content) {
 		def authorMatch = content =~ /(?s)<meta name="author" content="(.*?)" \/>/
-		return authorMatch[0][1]
+		if (authorMatch)
+			return authorMatch[0][1]
 	}
 
 	private def fetchTitle(content) {
 		def titleMatch = content =~ /(?s)<title>(.*?)<\/title>/
-		return titleMatch[0][1]
+		if (titleMatch)
+			return titleMatch[0][1]
 	}
-	
+
 	private fetchKeywords(content) {
 		def keywordMatch = content =~ /(?s)<meta name="keywords" content="(.*?)" \/>/
-		return keywordMatch[0][1]
+		if (keywordMatch)
+			return keywordMatch[0][1]
 	}
-	
+
 	private fetchRobots(content) {
 		def robotMatch = content =~ /(?s)<meta name="robots" content="(.*?)" \/>/
-		return robotMatch[0][1]
+		if (robotMatch)
+			return robotMatch[0][1]
 	}
 }
