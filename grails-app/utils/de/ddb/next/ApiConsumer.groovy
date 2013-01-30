5992b53e6fd43fa1cb96d07f3399e465aad56e55
Emiliano Masi <emiliano.masi@fiz-karlsruhe.de>
2013-01-30 12:32:58 +0100

    Wrapper + Adjustment ItemResult DONE

--- grails-app/utils/de/ddb/next/ApiConsumer.groovy
+++ grails-app/utils/de/ddb/next/ApiConsumer.groovy
@@ -4,8 +4,7 @@ import groovyx.net.http.ContentType
 import groovyx.net.http.Method
 import groovyx.net.http.RESTClient
 
-import static groovyx.net.http.ContentType.JSON
-import static groovyx.net.http.ContentType.URLENC
+import static groovyx.net.http.ContentType.*
 import groovy.json.*
 import org.codehaus.groovy.grails.web.json.JSONArray
 
@@ -20,16 +19,16 @@ class ApiConsumer {
 				uri.query = query
 				headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
 				response.success = { resp, reader ->
-//					println "response status: ${resp.statusLine}"
-//					println 'Headers: -----------'
-//					resp.headers.each { h ->
-//						println " ${h.name} : ${h.value}"
-//					}
+				println "response status: ${resp.statusLine}"
+				println 'Headers: -----------'
+				resp.headers.each { h ->
+					println " ${h.name} : ${h.value}"
+				}
 					ret = reader.getText()
 				}
-//				response.'404' = { 
-//					ret= 'Not found' 
-//				}
+				response.'404' = { 
+					ret= 'Not found' 
+				}
 				response.failure = { resp ->
 					println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
 					ret = 'Not found'
@@ -61,30 +60,76 @@ class ApiConsumer {
 				response.success = { resp, json ->
 					println "response status: ${resp.statusLine}"
 					println 'Headers: -----------'
-					/*
 					resp.headers.each { h ->
 						println " ${h.name} : ${h.value}"
-					}*/
+					}
 					ret = json
 				}
 				response.failure = { resp ->
 					println "response status: ${resp.statusLine}"
 					println 'Headers: -----------'
-					/*
+					
 					resp.headers.each { h ->
 						println " ${h.name} : ${h.value}"
-					}*/
+					}
 					println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
 				}
-//				response.'404' = {resp, reader ->
-//					println "response status: ${resp.statusLine}"
-//					println 'Headers: -----------'
-//					resp.headers.each { h ->
-//						println " ${h.name} : ${h.value}"
-//					}
-//					println "###########404"
-//					ret= 'Not found'
-//				}	
+				response.'404' = {resp, reader ->
+				println "response status: ${resp.statusLine}"
+				println 'Headers: -----------'
+				resp.headers.each { h ->
+					println " ${h.name} : ${h.value}"
+				}
+				println "###########404"
+				ret= 'Not found'
+			}	
+			}
+			return ret
+
+		} catch (groovyx.net.http.HttpResponseException ex) {
+			ex.printStackTrace()
+			return null
+		} catch (java.net.ConnectException ex) {
+			ex.printStackTrace()
+			return null
+		}
+	}
+	
+	static def getTextAsXml(String baseUrl, String path, query, method = Method.GET) {
+		try {
+			def ret = null
+			def http = new HTTPBuilder(baseUrl)
+			http.request(method, XML) {
+				uri.path = path
+				uri.query = query
+				headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
+				headers.Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+				response.success = { resp, xml ->
+					println "response status: ${resp.statusLine}"
+					println 'Headers: -----------'
+					resp.headers.each { h ->
+						println " ${h.name} : ${h.value}"
+					}
+					ret = xml
+				}
+				response.failure = { resp ->
+					println "response status: ${resp.statusLine}"
+					println 'Headers: -----------'
+					
+					resp.headers.each { h ->
+						println " ${h.name} : ${h.value}"
+					}
+					println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
+				}
+				response.'404' = {resp, reader ->
+				println "response status: ${resp.statusLine}"
+				println 'Headers: -----------'
+				resp.headers.each { h ->
+					println " ${h.name} : ${h.value}"
+				}
+				println "###########404"
+				ret= 'Not found'
+			}
 			}
 			return ret
 
