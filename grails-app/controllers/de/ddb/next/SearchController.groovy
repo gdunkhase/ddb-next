569f090417eed97ea79b1de184586b8d828e8ed4
Frank Schwichtenberg <Frank@Schwichtenberg.net>
2013-02-05 17:27:34 +0100

    QuickFix: fixed default limit 20, params q or query are possible

--- grails-app/controllers/de/ddb/next/SearchController.groovy
+++ grails-app/controllers/de/ddb/next/SearchController.groovy
@@ -1,25 +1,20 @@
 package de.ddb.next
-import java.util.List;
-
-import net.sf.json.JSONArray
-import org.apache.commons.lang.builder.ReflectionToStringBuilder
-import grails.converters.JSON
-import groovy.json.JsonSlurper
-import groovyx.net.http.HTTPBuilder
 
 
 class SearchController {
 
-  static defaultAction = "results"
+    static defaultAction = "results"
 
     def results() {
-    def query = [ client: "DDB-NEXT" ]
-    if (params.q!=null){
-      query = [ query: params.q ]
-    }
-    def results = ItemResult.getAllItemsResult(query, grailsApplication.config.ddb.wsItemResults.toString())
-    def pagesOverallIndex = message(code:"ddbnext.Page")+" 1 "+message(code:"ddbnext.Of")+" xxxxx"
+        def urlQuery = [ client: "DDB-NEXT" ]
+        if (params.q!=null || params.query!=null){
+            // FIXME remove q
+            urlQuery = (params.q)?[ query: params.q ]:[query: params.query]
+        }
+        urlQuery["rows"] = 20
+        def results = ItemResult.getAllItemsResult(urlQuery, grailsApplication.config.ddb.wsItemResults.toString())
+        def pagesOverallIndex = message(code:"ddbnext.Page")+" 1 "+message(code:"ddbnext.Of")+" xxxxx"
 
-    render(view: "results", model: [results: results, pagesOverallIndex: pagesOverallIndex])
-  }
+        render(view: "results", model: [results: results, pagesOverallIndex: pagesOverallIndex])
+    }
 }
