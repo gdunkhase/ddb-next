3d1483d1afa25d326c83d7dedb9c4decb02905ea
Andre Schenk <Andre.Schenk@fiz-Karlsruhe.de>
2013-02-06 18:55:55 +0100

    use backend URL from configuration

--- grails-app/taglib/de/ddb/next/BackendVersionTagLib.groovy
+++ grails-app/taglib/de/ddb/next/BackendVersionTagLib.groovy
@@ -3,6 +3,6 @@ package de.ddb.next
 class BackendVersionTagLib {
 	def backendVersion = {
 		attrs, body ->
-		out << ApiConsumer.getText("http://dev-backend.deutsche-digitale-bibliothek.de:9998", "/version", null)
+		out << ApiConsumer.getText(grailsApplication.config.ddb.backend.url, "/version", null)
 	}
 }
