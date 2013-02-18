f5ef5c9f935b21d1b7968a605236bb1d42177f29
aschenk <Andre.Schenk@fiz-Karlsruhe.de>
2013-02-18 17:27:55 +0100

    further work on item hierarchy

--- web-app/js/item-hierarchy.js
+++ web-app/js/item-hierarchy.js
@@ -1,58 +1,94 @@
 function createHierarchy(url) {
   getParents(url, url.file, function(parents) {
-    var list = $('<div>');
+    var list = $("<div>");
     var currentNode = list;
 
     $.each(parents.reverse(), function(index, value) {
       if (index < parents.length - 1) {
+        // show parents
         var parentId = null;
 
         if (index > 0) {
           parentId = parents[index - 1].id;
         }
-        currentNode = createNode(url, currentNode, parentId, value);
+        currentNode = createParentNode(url, currentNode, parentId, value);
       } else if (parents.length > 1) {
+        // show children
         getChildren(url, parents[parents.length - 2].id, function(children) {
-          var childList = $('<ul>');
+          var childList = $("<ul>");
+          var length = children.length;
 
           $.each(children, function(index, value) {
-            childList.append($('<li>').append($('<a>', {
-              href : value.id
-            }).append(value.label)));
+            createChildNode(childList, value, index == length - 1);
           });
           currentNode.append(childList);
         });
       }
     });
-    $('.item-hierarchy-result').html(list);
+    $(".item-hierarchy-result").html(list);
   });
 }
 
-function createNode(url, currentNode, parentId, value) {
-  var siblingCount = $('<span>', {
-    class : "siblingCount"
+function createChildNode(currentNode, value, isLast) {
+  var li = $("<li>", {
+    class : "leaf" + (isLast ? " last" : "")
+  });
+  var branchType = $("<span>", {
+    class : "branch-type"
+  });
+  var i = $("<i>");
+  var leafIndicator = $("<div>", {
+    class : "leaf-indicator"
+  });
+  var a = $("<a>", {
+    href : value.id
   });
 
+  branchType.append(i);
+  li.append(branchType);
+  a.append(value.label);
+  li.append(leafIndicator);
+  li.append(a);
+  currentNode.append(li);
+}
+
+function createParentNode(url, currentNode, parentId, value) {
+  var result = $("<li>", {
+    class : (parentId == null ? "root" : "node")
+  });
+  var siblingCount = $("<span>", {
+    class : "sibling-count"
+  });
+
+  // count siblings
   if (parentId != null) {
     getChildren(url, parentId, function(children) {
       if (children.length > 1) {
         siblingCount.append("+" + (children.length - 1));
+        result.addClass("more-hidden");
       }
     });
   }
 
-  var result = $('<li>').append($('<a>', {
-    href : value.id
-  }).append(value.label));
+  var branchType = $("<span>", {
+    class : "branch-type"
+  });
+  var i = $("<i>");
+  var label = $("<span>", {
+    class : "item-hierarchy-label"
+  }).append(value.label);
 
-  currentNode.append($('<ul>').append(siblingCount).append(result));
+  branchType.append(i);
+  result.append(branchType);
+  result.append(label);
+  currentNode.append($("<ul>").append(siblingCount).append(result));
   return result;
 }
 
 function getChildren(url, id, complete) {
   $.ajax({
-    type : 'GET',
-    dataType : 'json',
+    type : "GET",
+    dataType : "json",
     async : true,
     url : url.dir + "children/" + id,
     complete : function(data) {
@@ -63,8 +99,8 @@ function getChildren(url, id, complete) {
 
 function getParents(url, id, complete) {
   $.ajax({
-    type : 'GET',
-    dataType : 'json',
+    type : "GET",
+    dataType : "json",
     async : true,
     url : url.dir + "parents/" + id,
     complete : function(data) {
@@ -83,11 +119,11 @@ function parseUrl(url) {
 }
 
 window.onload = function() {
-  $('.item-hierarchy').removeClass('off');
+  $(".item-hierarchy").removeClass("off");
 
-  var imgLoader = document.createElement('img');
+  var imgLoader = document.createElement("img");
 
   imgLoader.src = "../images/icons/loader_small.gif";
-  $('.item-hierarchy-result').prepend(imgLoader);
+  $(".item-hierarchy-result").prepend(imgLoader);
   createHierarchy(parseUrl(this.location.href));
 }
