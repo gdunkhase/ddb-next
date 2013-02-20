efbb489d715b1bdd5a3f2af927a2bece7bfb8158
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2013-02-20 16:14:23 +0100

    add comment

--- web-app/js/institution-list.js
+++ web-app/js/institution-list.js
@@ -1,39 +1,46 @@
 'use strict';
 $(document).ready(function() {
   $('.filter').show();
-  
+
   var all = $('.institution');
   console.log('list: ', all); //.not(filtered).hide();
 
+  // "Filter" by section. It is *not* a reduction filter which combination of
+  // filters form a AND relation. The implemented filter has following semantic
+  // `show the user all institutions which match filter foo OR filter bar.
+  // TODO add class or id to improve readability
   $('input:checkbox').click(function() {
     if (this.checked) {
-      
+
+      // only load the data from the server once until page refreshes.
       if (ddb.institutions === null) {
         ddb.institutions = ddb.fetchData();
       }
-      
+
       var filtered = ddb.filterBySection(this);
-      
+
+      // highlight the filtered institutions
       $(filtered).find('a').addClass('highlight');
-      
-      // TODO: hide unfiltered
       console.log('filtered: ', filtered);
       
+      
+      // hide others
       var notFiltered = $(all).not(filtered);
       console.log('filtered: ', notFiltered);
-      
+
       $(all).not(filtered).hide();
-      
+
     } else {
-      
+      // if uncheck a section filter
+
       var filtered = ddb.filterBySection(this);
       $(filtered).find('a').removeClass('highlight');
-      
+
       var notFiltered = $(all).not(filtered);
       console.log('filtered: ', notFiltered);
-      
+
       $(all).not(filtered).show();
-      
+
     }
   });
 });
@@ -44,7 +51,7 @@ var ddb = {
     // TODO: move the url to an external configuration file.
     ddbBackendUrl: 'http://localhost:8080/apis/institutions'
   },
-  
+
   institutions: null,
 
   filterBySection: function(el) {
