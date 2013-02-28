16ec10605a838b949ed45b1d7835a7c037d60cd6
Christian Herlambang <christian.herlambang@fiz-karlsruhe.de>
2013-02-28 15:25:04 +0100

    [ddbnext-27] refactoring

--- web-app/js/institution-list.js
+++ web-app/js/institution-list.js
@@ -1,12 +1,9 @@
+// TODO: remove `use strict` via automate script in the production mode.
+// SEE: http://scriptogr.am/micmath/post/should-you-use-strict-in-your-production-javascript
 'use strict';
-$(document).ready(function() {
-  // if the User Agent enable JS, show the `filter by sector` checkboxes.
-  $('.filter').show();
 
-  // pass callback function onHashChange, onFilterBySectorChange
-  ddb.fetchData(ddb.onHashChange, ddb.onFilterBySectorChange);
-});
-
+// TODO: use other pattern for namespace in JS
+// SEE: http://enterprisejquery.com/2010/10/how-good-c-habits-can-encourage-bad-javascript-habits-part-1/
 var ddb = {
 
   Config: {
@@ -15,24 +12,23 @@ var ddb = {
 
   all: $('.institution'),
 
-  institutions: null,
+  institutionsBySector: null,
 
+  // TODO: combine `filter by sector` and `filter by first letter`
+  // TODO: refactor the function to smaller functions
   onHashChange: function() {
     var hash = window.location.hash.substring(1);
-    console.log('hash change to: ' + hash);
 
     if (hash === 'All' || hash === 'ALL' || hash === 'list') {
       ddb.firstLetterFilter = '';
-      console.log('show all');
       if (ddb.filteredEl) {
 
-        // view manipulation
-        // reset to initial state
+        // view manipulation, it resets the view to the initial state.
         ddb.filteredEl.css('background-color', 'white');
         ddb.restEl.css('background-color', 'white');
       }
-    } else if (ddb.institutions[hash]) {
-      var idList = _.pluck(ddb.institutions[hash], 'id');
+    } else if (ddb.institutionsBySector[hash]) {
+      var idList = _.pluck(ddb.institutionsBySector[hash], 'id');
       // find all institutions match idList
       ddb.filteredEl = ddb.all.filter(function() {
         return _.contains(idList, $(this).data('institution-id'));
@@ -40,21 +36,19 @@ var ddb = {
 
       // find all first level institutions which are not start with
       // firstLetter
-      var restKeys = _.chain(ddb.institutions)
+      var restKeys = _.chain(ddb.institutionsBySector)
         .keys()
         .reject(function(key) { return key === hash; })
         .value();
 
       // get all values from restKeys
-      var restIdList = _.chain(ddb.institutions)
-        .filter(function(val, key) {
-          return _.contains(restKeys, key);
-        })
+      var restIdList = _.chain(ddb.institutionsBySector)
+        .filter(function(val, key) { return _.contains(restKeys, key); })
         .flatten()
         .pluck('id')
         .value();
 
-      // collect all ids and return as array
+      // collect the HTML elements that match id in the restIdList
       ddb.restEl = ddb.all.filter(function() {
         return _.contains(restIdList, $(this).data('institution-id'));
       });
@@ -63,179 +57,114 @@ var ddb = {
       ddb.filteredEl.css('background-color', 'red');
       ddb.restEl.css('background-color', 'yellow');
     } else {
+      // view manipulation
       $('.pagination a').removeClass('selected');
       $('.pagination a[href="' + window.location.hash + '"]')
-        .addClass('selected');
+      .addClass('selected');
     }
   },
 
   onFilterBySectorChange: function() {
     $('input:checkbox').click(function() {
-      console.log('filter selected');
+      // flatten the object of arrays => to the arrays of objects
+      // TODO: do it only once, not by every function calls.
+      var institutionList = _.chain(ddb.institutionsBySector)
+        .values()
+        .flatten()
+        .value();
 
-      var checked = $('.sector-facet input:checked');
-      var sector = $(this).data('sector');
+      // TODO: not used, remove this
+      var allSelectedSectors = $('.sector-facet input:checked');
 
-     // flatten the map of arrays => arrays of objects
-     // TODO: find better solution.
-     var list = [];
-     _.each(ddb.institutions, function(value, key) {
-       list.push(value);
-     });
-     list = _.flatten(list);
+      var selectedSector = $(this).data('sector');
 
-     // by objects which match sector, return as array
-     var parents = [];
-     var filtered = _.reduce(list, function(memory, object) {
-       if (object.sector === sector) {
-         memory.push(object);
-       }
+      var parentList = [];
+      var filteredBySector = _.reduce(institutionList, function(memory, institution) {
+        if (institution.sector === selectedSector) {
+          memory.push(institution);
+        }
 
-       if (object.children && object.children.length > 0) {
-         _.reduce(object.children, function(otherMemory, child) {
-           if (child.sector === sector) {
-             otherMemory.push(child);
-             parents.push(object);
-           }
-           return otherMemory;
-         }, memory);
-       }
+        if (institution.children && institution.children.length > 0) {
+          _.reduce(institution.children, function(otherMemory, child) {
+            if (child.sector === selectedSector) {
+              otherMemory.push(child);
+              // collect the parent
+              // TODO: refactor this, it insert the same parent multiple
+              // times.
+              parentList.push(institution);
+            }
+          return otherMemory;
+          }, memory);
+        }
+        return memory;
+      }, []);
 
-       return memory;
-     }, []);
-     parents = _.uniq(parents);
+      // TODO: it should not be nesecassry
+      parentList = _.uniq(parentList);
 
+      /*
+       console.log('<pre>' + JSON.stringify(filteredBySector, null, 4) + '</pre>');
+      */
+      var visible = _.union(parentList, filteredBySector);
 
-     /*
-     console.log('<pre>' + JSON.stringify(filtered, null, 4) + '</pre>');
-     console.log(filtered.length);
-     */
-
-     var visible = _.union(parents, filtered);
-
-     // TODO: disable/enable index
-     var hasNoMember = _.reduce(ddb.institutions, function(memo, array, key) {
+     // disable/enable index
+     var hasNoMember = _.reduce(ddb.institutionsBySector, function(memo, array, key) {
        if (_.intersection(array, visible).length === 0) {
          memo.push(key);
        }
        return memo;
      }, []);
 
-     console.log(hasNoMember);
+      var invisible = _.without(institutionList, visible);
+      console.log('# first level institution: ' + institutionList.length);
+      console.log('# filtered by sector: ' + filteredBySector.length);
+      console.log('# visible: filtered + parents: ' + visible.length);
+      console.log('# rest: ' + institutionList.length);
 
-     // find all institutions match idList
-     var matched = ddb.all.filter(function() {
-       return _.contains(_.pluck(filtered, 'id'),
-                         $(this).data('institution-id'));
-     });
-     matched.css('color', 'red');
+      // console.log('<pre>' + JSON.stringify(invisible, null, 4) + '</pre>');
 
-     var visibleEl = ddb.all.filter(function() {
-       return _.contains(_.pluck(visible, 'id'),
-                         $(this).data('institution-id'));
-     });
+     // view manipulation
+     var filteredBySectorEl = ddb.findElements(filteredBySector);
+     filteredBySectorEl.toggleClass('highlight');
+
+     var visibleEl = ddb.findElements(visible);
      visibleEl.css('background-color', 'green');
 
-     _.each(hasNoMember, function(letter) {
+     var invisibleEl = ddb.findElements(invisible);
+     invisibleEl.css('background-color', 'blue');
+
+    // update index view, i.e., A..Z
+    _.each(hasNoMember, function(letter) {
       $('.pagination a[href="' + '#' + letter + '"]').parent()
-        .addClass('disabled');
-     });
-
+      .addClass('disabled');
     });
-  },
+  });
+},
 
-  fetchData: function(onHashChange, onFilterBySectorChange) {
-    if (ddb.institutions === null) {
-      $.getJSON(ddb.Config.ddbBackendUrl, function(data) {
-        console.log('the institution: ', data);
-        ddb.institutions = data;
+findElements: function(list) {
+  return ddb.all.filter(function() {
+    return _.contains(_.pluck(list, 'id'),
+    $(this).data('institution-id'));
+  });
+},
 
-        // call the callbacks, once data is loaded.
-        onHashChange();
-        onFilterBySectorChange();
-        window.onhashchange = onHashChange;
-      });
-    }
+getInstitutionsBySector: function(onHashChange, onFilterBySectorChange) {
+  if (ddb.institutionsBySector === null) {
+    $.getJSON(ddb.Config.ddbBackendUrl, function(data) {
+      ddb.institutionsBySector = data;
+
+      // call the callbacks, once data is loaded.
+      onHashChange();
+      onFilterBySectorChange();
+      window.onhashchange = onHashChange;
+    });
   }
+}
 };
 
+$(function() {
+  // if the User Agent enables JS, show the `filter by sector` checkboxes.
+  $('.filter').show();
 
-/*
-  var all = $('.institution');
-  var allSelected = [];
-  var rest;
-
-  // find the current path
-  ddb.windowHash = window.location.hash;
-  ddb.filterByHash(ddb.windowHash.substring(1), allSelected, all);
-
-  // "Filter" by section. It is *not* a reduction filter which combination of
-  // filters form a AND relation. The implemented filter has following semantic
-  // `show the user all institutions which match filter foo OR filter bar.
-  // TODO add class or id to improve readability
-  $('input:checkbox').click(function() {
-
-    // the user check one of the filter ==> select
-    // TODO: if institution has descendants, show all descendants too but don't
-    // highlight them.
-    if (this.checked) {
-      // the logic
-      var currentSelected = ddb.filterBySection(this);
-      allSelected = $.merge(allSelected, currentSelected);
-      rest = $(all).not(allSelected);
-
-      // manipulate the view
-      // highlight the current selection
-      $(currentSelected).find('> a').addClass('highlight');
-      currentSelected.show();
-      rest.hide();
-    } else {
-      // the user uncheck one of the filter ==> remove selection
-
-      // the logic
-      var removedSelection = ddb.filterBySection(this);
-      allSelected = $(allSelected).not(removedSelection);
-
-      rest = $(all).not(allSelected);
-
-      // the view manipulation
-      // un-highlight the selected.
-      removedSelection.find('a').removeClass('highlight');
-      removedSelection.hide();
-
-      // when all filtered are removed, show all.
-      // TODO: the performace is bad, when the user removes all filters.
-      if ($(allSelected).length === 0) {
-        $(all).show();
-      }
-    }
-  });
-
-  // TODO add listener for first letter index.
-  // TODO when `All` is selected, show all institutions.
-  // TODO when an index is selected.
-  // 1. show all institution starting with the selected index character.
-  // 2. hide the rest = {all} \ {selected}
-  $('div.pagination a').click(function(e) {
-    e.preventDefault();
-
-    var selectedHash = e.target.innerHTML;
-
-    // TODO: bad performance
-    if (selectedHash === 'All') {
-      $(all).show();
-    } else {
-      if (allSelected.length === 0) {
-        var filteredByFirstLetter = $(all).filter(function() {
-          var firstLetter = $(this).data('first-letter');
-          return firstLetter === selectedHash;
-        });
-
-        filteredByFirstLetter.show();
-        $(all).not(filteredByFirstLetter).hide();
-      } else {
-        ddb.filterByHash(selectedHash, allSelected);
-      }
-    }
-  });
-*/
+  ddb.getInstitutionsBySector(ddb.onHashChange, ddb.onFilterBySectorChange);
+});
