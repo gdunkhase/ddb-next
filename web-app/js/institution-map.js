0cea25d527338822805050ce1c647dc3e66b62be
MichaelHoppe <michael.hoppe@fiz-karlsruhe.de>
2013-03-21 17:07:26 +0100

    add click-event

--- web-app/js/institution-map.js
+++ web-app/js/institution-map.js
@@ -7,4 +7,15 @@ window.ddbAddOnloadListener(function() {
 function mapInitializer(){
   $('#institution-map').removeClass('off');
   $('#institution-list').addClass('off');
+  
+  $('input:checkbox').click(function() {
+      applyFilters();
+  });
+  
+  function applyFilters() {
+    var sectors = ddb.getSelectedSectors();
+    alert(sectors);
+  }
+
+
 };
