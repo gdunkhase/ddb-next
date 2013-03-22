6057d0f54a722c4ded26bdb016e1611422a7cce8
Emiliano Masi <emiliano.masi@fiz-karlsruhe.de>
2013-03-22 09:03:21 +0100

    onload to on dom ready

--- web-app/js/onloadManager.js
+++ web-app/js/onloadManager.js
@@ -1,23 +1,21 @@
 window.ddbOnloadListenerQueue = new Array();
 
-window.ddbAddOnloadListener = function(listener){
-	window.ddbOnloadListenerQueue.push(listener);
+window.ddbAddOnloadListener = function(listener) {
+    window.ddbOnloadListenerQueue.push(listener);
 };
 
-window.ddbNotifyOnloadListener = function(){
-	for(var i=0;i<window.ddbOnloadListenerQueue.length;i++){
-		try{
-			window.ddbOnloadListenerQueue[i]();
-		}catch(error){
-			if (typeof console != "undefined"){
-				console.log(error);
-			}
-		}
-	}
+window.ddbNotifyOnloadListener = function() {
+    for ( var i = 0; i < window.ddbOnloadListenerQueue.length; i++) {
+        try {
+            window.ddbOnloadListenerQueue[i]();
+        } catch (error) {
+            if (typeof console != "undefined") {
+                console.log(error);
+            }
+        }
+    }
 };
 
 $(document).ready(function() {
-	window.ddbNotifyOnloadListener();
+    window.ddbNotifyOnloadListener();
 });
-
-
