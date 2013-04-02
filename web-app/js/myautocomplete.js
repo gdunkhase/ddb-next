efe1c1a18d382a633ba21dc8801365a3a0eb9a8b
Ar Brahaj <armand.brahaj@fiz-karlsruhe.de>
2013-04-02 15:32:26 +0200

    new Branch DDBNEXT-276

--- web-app/js/myautocomplete.js
+++ web-app/js/myautocomplete.js
@@ -14,35 +14,33 @@
  * limitations under the License.
  */
 $(function() {
-	$('input.query').autocomplete(
-			{
-				source : function(request, response) {		
-					$.ajax(
-						{
-						url : "http://backend.deutsche-digitale-bibliothek.de:9998/search/suggest/",
-						url : "/apis/autocomplete/",
-						dataType : "jsonp",
-						data : {
-							query : request.term
-						},
-						success : function(data) {
-							response($.map(data, function(n,i) {
-								return {
-									label : n.substring(0,45),
-									value : n
-								}
-							}));
-						}
-					});
-				},
-				minLength : 2,
-				open : function() {
-					$(this).removeClass("ui-corner-all").addClass(
-							"ui-corner-top");
-				},
-				close : function() {
-					$(this).removeClass("ui-corner-top").addClass(
-							"ui-corner-all");
+  $('input.query').autocomplete(
+    {
+	  source : function(request, response) {		
+		$.ajax(
+		  {
+			url : "http://backend.deutsche-digitale-bibliothek.de:9998/search/suggest/",
+			url : "/apis/autocomplete/",
+			dataType : "jsonp",
+			data : {
+			  query : request.term
+			},
+			success : function(data) {
+			  response($.map(data, function(n,i) {
+			    return {
+				  label : n.substring(0,45),
+				  value : n
 				}
-			});
+			  }));
+			}
+		});
+      },
+	  minLength : 2,
+	  open : function() {
+	    $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+	  },
+	  close : function() {
+	    $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+	  }
+	});
 });
