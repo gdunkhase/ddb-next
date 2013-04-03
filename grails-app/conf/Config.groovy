0b31173804c5e90cfcf880a908acf3fc07c985ee
hla <holger.lauinger@fiz-karlsruhe.de>
2013-04-03 09:45:47 +0200

    HOTFIX: Removed the PDF rendering config "grails.serverURL" due to critical side effects on link generation.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -105,7 +105,7 @@ ddb.advancedSearch.defaultRows=20
 
 
 // The grails.serverURL is required for the PDF rendering plugin.
-grails.serverURL=ddb.apis.url
+//grails.serverURL=ddb.apis.url // hla: Temporarily removed due to side effects on link generation
 
 grails.resources.mappers.zip.excludes = [
     '**/*.png',
