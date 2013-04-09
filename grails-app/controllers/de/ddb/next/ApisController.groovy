0f9d6eef7685684ce20b2431edebb03c8eed21a8
Ar Brahaj <armand.brahaj@fiz-karlsruhe.de>
2013-04-09 11:15:03 +0200

    Support cache control on download of binary resources

--- grails-app/controllers/de/ddb/next/ApisController.groovy
+++ grails-app/controllers/de/ddb/next/ApisController.groovy
@@ -15,80 +15,81 @@
  */
 package de.ddb.next
 
-import java.util.regex.Pattern;
-
 import net.sf.json.JSONNull
+
 import grails.converters.JSON
-import groovy.json.JsonSlurper
+import java.text.SimpleDateFormat
 
 class ApisController {
 
-    def apisService
+	def apisService
 
-    def search(){
+	def search(){
 
-        def resultList = [:]
-        def facets = []
-        def highlightedTerms = []
-        def docs = []
-        def query = apisService.getQueryParameters(params)
+		def resultList = [:]
+		def facets = []
+		def highlightedTerms = []
+		def docs = []
+		def query = apisService.getQueryParameters(params)
 
-        def jsonResp = ApiConsumer.getTextAsJson(grailsApplication.config.ddb.backend.url.toString(),'/search', query)
-        jsonResp.results["docs"].get(0).each{
+		def jsonResp = ApiConsumer.getTextAsJson(grailsApplication.config.ddb.backend.url.toString(),'/search', query)
+		jsonResp.results["docs"].get(0).each{
 
-            def tmpResult = [:]
-            def title
-            def subtitle
-            def thumbnail
-            def media = []
+			def tmpResult = [:]
+			def title
+			def subtitle
+			def thumbnail
+			def media = []
 
-            def titleMatch = it.preview.toString() =~ /(?m)<div (.*?)class="title"(.*?)>(.*?)<\/div>$/
-            if (titleMatch)
-                title= titleMatch[0][3]
+			def titleMatch = it.preview.toString() =~ /(?m)<div (.*?)class="title"(.*?)>(.*?)<\/div>$/
+			if (titleMatch)
+				title= titleMatch[0][3]
 
-            def subtitleMatch = it.preview.toString() =~ /(?m)<div (.*?)class="subtitle"(.*?)>(.*?)<\/div>$/
-            subtitle= (subtitleMatch)?subtitleMatch[0][3]:""
+			def subtitleMatch = it.preview.toString() =~ /(?m)<div (.*?)class="subtitle"(.*?)>(.*?)<\/div>$/
+			subtitle= (subtitleMatch)?subtitleMatch[0][3]:""
 
-            def thumbnailMatch = it.preview.toString() =~ /(?m)<img (.*?)src="(.*?)"(.*?)\/>$/
-            if (thumbnailMatch)
-                thumbnail= thumbnailMatch[0][2]
+			def thumbnailMatch = it.preview.toString() =~ /(?m)<img (.*?)src="(.*?)"(.*?)\/>$/
+			if (thumbnailMatch)
+				thumbnail= thumbnailMatch[0][2]
 
-            def mediaMatch = it.preview.toString() =~ /(?m)<div (.*?)data-media="(.*?)"/
-            if (mediaMatch)
-                mediaMatch[0][2].split (",").each{ media.add(it) }
+			def mediaMatch = it.preview.toString() =~ /(?m)<div (.*?)data-media="(.*?)"/
+			if (mediaMatch)
+				mediaMatch[0][2].split (",").each{ media.add(it) }
 
-            tmpResult["id"] = it.id
+			tmpResult["id"] = it.id
 
-            tmpResult["view"] = (it.view instanceof JSONNull)?"":it.view
-            tmpResult["label"] = (it.label instanceof JSONNull)?"":it.label
-            tmpResult["latitude"] = (it.latitude instanceof JSONNull)?"":it.latitude
-            tmpResult["longitude"] = (it.longitude instanceof JSONNull)?"":it.longitude
-            tmpResult["category"] = (it.category instanceof JSONNull)?"":it.category
+			tmpResult["view"] = (it.view instanceof JSONNull)?"":it.view
+			tmpResult["label"] = (it.label instanceof JSONNull)?"":it.label
+			tmpResult["latitude"] = (it.latitude instanceof JSONNull)?"":it.latitude
+			tmpResult["longitude"] = (it.longitude instanceof JSONNull)?"":it.longitude
+			tmpResult["category"] = (it.category instanceof JSONNull)?"":it.category
 
-            def properties = [:]
+			def properties = [:]
 
-            tmpResult["preview"] = [title:title, subtitle: subtitle, media: media, thumbnail: thumbnail]
-            tmpResult["properties"] = properties
-            docs.add(tmpResult)
-        }
-        if(jsonResp.results["docs"].get(0).size()>0){
-            apisService.fetchItemsProperties(jsonResp.results["docs"].get(0)).eachWithIndex() {
-              obj, i ->
-              docs[i].properties = obj
-            }
-        }
-        resultList["facets"] = jsonResp.facets
-        resultList["highlightedTerms"] = jsonResp.highlightedTerms
-        resultList["results"] = [name:jsonResp.results.name,docs:docs,numberOfDocs:jsonResp.results.numberOfDocs]
-        resultList["numberOfResults"] = jsonResp.numberOfResults
-        resultList["randomSeed"] = jsonResp.randomSeed
-
+			tmpResult["preview"] = [title:title, subtitle: subtitle, media: media, thumbnail: thumbnail]
+			tmpResult["properties"] = properties
+			docs.add(tmpResult)
+		}
+		if(jsonResp.results["docs"].get(0).size()>0){
+			apisService.fetchItemsProperties(jsonResp.results["docs"].get(0)).eachWithIndex() { obj, i ->
+				docs[i].properties = obj
+			}
+		}
+		resultList["facets"] = jsonResp.facets
+		resultList["highlightedTerms"] = jsonResp.highlightedTerms
+		resultList["results"] = [name:jsonResp.results.name,docs:docs,numberOfDocs:jsonResp.results.numberOfDocs]
+		resultList["numberOfResults"] = jsonResp.numberOfResults
+		resultList["randomSeed"] = jsonResp.randomSeed
 		render (contentType:"text/json"){resultList}
-    }
+	}
 
-	
+	def institutionsmap(){
+		def jsonResp = ApiConsumer.getTextAsJson(grailsApplication.config.ddb.backend.url.toString(),'/institutions/map', params)
+		render (contentType:"text/json"){jsonResp}
+	}
+
 	/**
-	 * This function should be obsolete once the 
+	 * This function should be obsolete once the
 	 * url : "http://backend.deutsche-digitale-bibliothek.de:9998/search/suggest/", would support JSONP and return the callback function
 	 * If that happens, the "myautocomplete.js" script should refer to the backend URL and not to this URL.
 	 * @return
@@ -96,7 +97,7 @@ class ApisController {
 	def autocomplete (){
 		def query = apisService.getQueryParameters(params)
 		def callback = apisService.getQueryParameters(params)
-		def result = ApiConsumer.getTextAsJson(grailsApplication.config.ddb.backend.search.autocomplete.url.toString(),'/search/suggest', query)	
+		def result = ApiConsumer.getTextAsJson(grailsApplication.config.ddb.backend.search.autocomplete.url.toString(),'/search/suggest', query)
 		if (callback) {
 			render "${params.callback}(${result as JSON})"
 		} else {
@@ -105,27 +106,55 @@ class ApisController {
 	}
 
 	/**
-	 * Wrapper to support streaming of files from the backend	
+	 * Wrapper to support streaming of files from the backend
 	 * @return OutPutStream
 	 */
 	def binary(){
 		def query = [ client: "DDB-NEXT" ]
-		def urlResponse= ApiConsumer.getBinaryContent(getBinaryServerUrl(),getFileNamePath(),query );
-		byte[] bytes=urlResponse.get("bytes");
+		def urlResponse= ApiConsumer.getBinaryContent(getBinaryServerUrl(),getFileNamePath(),query )
+		byte[] bytes=urlResponse.get("bytes")
+		response.setHeader("Cache-Control", "max-age=86400")
+		response.setHeader("Expires", formatDateForExpiresHeader(1).toString())
 		response.setContentType(urlResponse.get("Content-Type"))
 		response.setContentLength(urlResponse.get("Content-Length").toInteger())
 		response.setHeader("Content-Disposition", "inline; filename="+getFileNamePath().tokenize('/')[-1])
 		response.outputStream << bytes
 	}
-	
+
 	private def getBinaryServerUrl(){
 		def url = grailsApplication.config.ddb.binary.backend.url
 		assert url instanceof String, "This is not a string"
-		return url;
+		return url
+	}
+
+	def staticFiles() {
+		def query = [ client: "DDB-NEXT" ]
+		def urlResponse = ApiConsumer.getBinaryContent(grailsApplication.config.ddb.static.url,
+				'/static/' + getFileNamePath(), query )
+		if(urlResponse && urlResponse != 'Not found') {
+			byte[] bytes = urlResponse.get("bytes")
+			response.setContentType(urlResponse.get("Content-Type"))
+			response.setContentLength(urlResponse.get("Content-Length").toInteger())
+			response.setHeader("Content-Disposition", "inline; filename=" + getFileNamePath().tokenize('/')[-1])
+			response.outputStream << bytes
+		}
 	}
 	
-	private def getFileNamePath(){
-		String flNamePath = cleanHtml(params.filename, 'none')
-		return flNamePath
+	/**
+	 *  Format RFC 2822 date
+	 *  @parameters daysfromtoday, how many days from today do you want the date to be shifted
+	 *  @return date 
+	 */
+	private def formatDateForExpiresHeader(daysfromtoday=4){
+		def tomorrow= new Date()+daysfromtoday
+		String pattern = "EEE, dd MMM yyyy HH:mm:ss Z";
+		SimpleDateFormat format = new SimpleDateFormat(pattern, Locale.ENGLISH);
+		Date date = format.parse(String.format('%ta, %<te %<tb %<tY %<tT CET', tomorrow));
+		log.info("WHAT DATE IS TODAY!??? : ->>>>>>>>>>>>>>"+date)
+		return date
+	}
+
+	private def getFileNamePath() {
+		return cleanHtml(params.filename, 'none')
 	}
 }
