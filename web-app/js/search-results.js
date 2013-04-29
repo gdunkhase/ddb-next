2bff5bbec33dfe3938e102570fd5cac266f23572
Emiliano Masi <emiliano.masi@fiz-karlsruhe.de>
2013-04-29 12:39:26 +0200

    ready

--- web-app/js/search-results.js
+++ web-app/js/search-results.js
@@ -15,7 +15,6 @@
  */
 //IMPORTANT FOR MERGING: This is the main function that has to be called when we are in the search results page
 $(function() {
-    console.log("ciao")
   if (window.history && history.pushState) {
     historyedited = false;
     historySupport = true;
