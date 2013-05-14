442d1472018704f78ffa7de96c9a9d967200a019
hla <holger.lauinger@fiz-karlsruhe.de>
2013-05-14 15:13:00 +0200

    HOTFIX: finetuning jwplayer

--- web-app/js/binaries-viewer.js
+++ web-app/js/binaries-viewer.js
@@ -51,9 +51,12 @@ $(document).ready(function() {
   function initializeJwPlayer(divId, videoFile, previewImage, width, height, onReadyCallback, onErrorCallback) {
     jwplayer(divId).setup({
       'flashplayer': jsContextPath + '/jwplayer/jwplayer.flash.swf',
+      'html5player': jsContextPath + '/jwplayer/jwplayer.html5.js',
       'modes': [{type: "html5", src: jsContextPath + "/jwplayer/jwplayer.html5.js"}, 
                 {type: "flash", src: jsContextPath + "/jwplayer/jwplayer.flash.swf"}, 
                 {type: "download"}],
+      'fallback': true,
+      'autostart': false,
       'file': videoFile,
       'skin': jsContextPath + '/jwplayer/skins/five.xml',
       'image': previewImage,
