6a001f3da61d26567f09310cde7a91f5a9106724
mih <michael.hoppe@fiz-karlsruhe.de>
2013-07-01 10:39:52 +0200

    add ddb.self.base.url to config

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -130,6 +130,7 @@ environments {
 
 //DDB SPECIFIC Configuration variables
 //The variables have to be overwritten by defining local configurations, see below environments
+ddb.self.base.url="http://localhost:8080"
 ddb.binary.url="http://localhost/binary/"
 ddb.static.url="http://localhost/static/"
 ddb.apis.url="http://localhost:8080/"
@@ -144,9 +145,6 @@ ddb.advancedSearch.searchFieldCount=10
 ddb.advancedSearch.defaultOffset=0
 ddb.advancedSearch.defaultRows=20
 ddb.session.timeout=1800 // in sec -> 30min
-ddb.passwordreset.confirmation="http://localhost:8080/ddb-next/user/confirm/|id|/|confirmationToken|?type=passwordreset"
-ddb.emailupdate.confirmation="http://localhost:8080/ddb-next/user/confirm/|id|/|confirmationToken|?type=emailupdate"
-ddb.create.confirmation="http://localhost:8080/ddb-next/user/confirm/|id|/|confirmationToken|?type=create"
 
 ddb.loadbalancer.header.name="nid"
 ddb.loadbalancer.header.value="-1"
