431cd3a1105fdf85b3197ad62c9bb17fe0befdcf
ttridico <tiziano.tridico@fiz-karlsruhe.de>
2013-07-03 09:02:09 +0200

    DDBNEXT-387: Institution popup, now only the matching institutions are counted.

--- web-app/third-party/map/geotemco_InstitutionsMap.js
+++ web-app/third-party/map/geotemco_InstitutionsMap.js
@@ -9419,7 +9419,7 @@ function PlacenamePopup(parent) {
                        );
         };
 
-        var nb = countHolder.length; // >= elementInfos.length;
+        var nb = elementInfos.length; // <= countHolder.length;
         var instText = "";
         if (nb == 1) {
             instText = GeoTemConfig.getString('institution');
